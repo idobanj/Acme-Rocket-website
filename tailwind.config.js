b966272de7f9ef58ba196a18bd9e6a38e51d0f54
peter <idowuolabanji181@gmail.com>
2025-11-14 13:22:15 +0100

    added contact section and also configure tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
- /** @type {import('tailwindcss').Config} */
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
 export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            screens: {
+                widescreen: {raw: '(min-aspect-ratio: 3/2)'},
+                tallscreen: {raw: '(min-aspect-ratio: 3/2)'},
+            },
+        },
+    },
+    plugins: [],
+};
