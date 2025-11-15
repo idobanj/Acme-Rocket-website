0fdc2172d627fc614edf258cb8d8b53b5878bdd2
peter <idowuolabanji181@gmail.com>
2025-11-15 11:48:52 +0100

    settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,21 @@
  */
 
 export default {
-    content: ['./src/**/*.{html,js}'],
-    theme: {
-        extend: {
-            screens: {
-                widescreen: {raw: '(min-aspect-ratio: 3/2)'},
-                tallscreen: {raw: '(min-aspect-ratio: 3/2)'},
-            },
-        },
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      screens: {
+        widescreen: { raw: '(min-aspect-ratio: 3/2)' },
+        tallscreen: { raw: '(min-aspect-ratio: 13/20)' },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        }
+      }
     },
-    plugins: [],
+  },
+  plugins: [],
 };
